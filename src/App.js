3f26e43825179dd6884b105eabc688d7e186ddbf
technicolor1 <gunsofwar999@gmail.com>
2018-05-10 10:38:10 -0400

    -major refactor of searchbox
    -break down to smaller components

--- src/App.js
+++ src/App.js
@@ -18,28 +18,18 @@ export class App extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         searchBoxInput: '',
          location: '',
          weatherData: ''
       }
 
-      this.handleChange = this.handleChange.bind(this);
       this.fetchLocation = this.fetchLocation.bind(this);
-      this.handleGeo = this.handleGeo.bind(this);
+      this.fetchWeather = this.fetchWeather.bind(this);
+      this.validateGoogle = this.validateGoogle.bind(this);
    }
 
-   handleChange(event) {
-      this.setState({
-         searchBoxInput: event.target.value
-      })
-   }
-
-   fetchLocation(val) {
-      if (val === "") {
-         console.log("Searchbox can't be empty");
-         return;
-      }
-      let google = `https://maps.googleapis.com/maps/api/geocode/json?address=${val}&key=${keys.google}`;
+   fetchLocation(uri) {
+      let google = `https://maps.googleapis.com/maps/api/geocode/${uri}&key=${keys.google}`;
+      console.log(google);
 
       fetch(google)
          .then(response => {
@@ -48,11 +38,12 @@ export class App extends React.Component {
             }
          })
          .then(data => {
+            console.log(data);
             this.validateGoogle(data);
          })
-
    }
 
+   
    // validate data from fetched google,
    validateGoogle(data) {
       // results must be a city or zipcode
@@ -60,43 +51,42 @@ export class App extends React.Component {
          locality: ["locality", "political"],
          zip: ["postal_code"]
       }
-
+      
       let found = null;
-
+      
       if (data.status === "ZERO_RESULTS") {
          console.log("Location can't be found");
          return;
       }
-
+      
       for (let i = 0; i < data.results.length; i++) {
          if ((data.results[i].types).includes(validObj.locality[0]) && (data.results[i].types).includes(validObj.locality[1])) {
             found = data.results[i];
             break;
-
+            
          } else if ((data.results[i].types).includes(validObj.zip[0])) {
             found = data.results[i];
             break;
-
+            
          }
       }
-
+      
       if (found !== null) {
          this.fetchWeather(found.geometry.location.lat, found.geometry.location.lng, found.formatted_address);
       } else {
-         console.log("Try a different query");
+         console.log("Doesn't match validObj (locality or zip)");
          return;
       }
    }
-
+   
    fetchWeather(lat, long, location) {
-      let proxy = "https://cors-anywhere.herokuapp.com/";
       let darksky = `https://api.darksky.net/forecast/${keys.opendarksky}/${lat},${long}?exclude=flags&callback=?`;
 
       $.getJSON(darksky, (data) => {
-         console.log("retrieving");
+         console.log("obtained");
       })
          .done(data => {
-            console.log(data);
+            console.log("processing", data);
 
             // cache fetched data, stringified
             localStorage.setItem("weatherData", JSON.stringify(data));
@@ -111,38 +101,7 @@ export class App extends React.Component {
             console.log(error);
          })
    }
-
-   handleGeo() {
-      let App = this;
-      function success(pos) {
-         let google = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${pos.coords.latitude},${pos.coords.longitude}&key=${keys.google}`;
-         fetch(google)
-            .then(response => {
-               if (response.ok) {
-                  return response.json();
-               }
-            })
-            .then(data => {
-               console.log(data);
-               App.validateGoogle(data);
-            })
-      }
-
-      function fail(error) {
-         console.log(error);
-      }
-
-      let options = {
-         timeout: 7500
-      }
-
-      navigator.geolocation.getCurrentPosition(success, fail, options);
-   }
-
-   shouldComponentUpdate() {
-      return true;
-   }
-
+   
    componentDidMount() {
       window.onload = () => {
          // if cached data present, use it instead of placeholder
@@ -169,7 +128,7 @@ export class App extends React.Component {
             
             <Header location={this.state.location} />
 
-            <SearchBox locateCall={this.fetchLocation} geoCall={this.handleGeo} />            
+            <SearchBox fetchLocation={this.fetchLocation} />            
 
             <Time time={this.state.weatherData.currently} />
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -5,61 +5,149 @@ export class SearchBox extends React.Component {
       super(props)
 
       this.state = {
-         searchBoxInput: ''
+         searchBoxValue: ''
       }
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleKeyPress = this.handleKeyPress.bind(this);
-      this.highlightText = this.highlightText.bind(this);
-      this.handleSumbitLocation = this.handleSumbitLocation.bind(this);
-      this.handleSubmitGeolocation = this.handleSubmitGeolocation.bind(this);
+      this.handleSearchBoxInputChange = this.handleSearchBoxInputChange.bind(this);
+      this.handleGeoBtnClicked = this.handleGeoBtnClicked.bind(this);
+      this.handleUserInputs = this.handleUserInputs.bind(this);
    }
 
-   highlightText(event) {
-      event.target.select();
-   }
+   // btn clicked or enter key pressed, all will be handled by one function 
+   // each will have a 'which' prop lifted up to determine the correct uri
+   // for Locatebtn and enter key, validate searchbox before uri 
+   // then lift uri up to parent App
 
-   handleChange(event) {
+   validateSearchboxInput() {    
+      if (this.state.searchBoxValue === "") {
+         console.log("Searchbox can't be empty");
+         return false;
+      }
+
+      return true;
+   }
+   
+   handleUserInputs() {
+      if (!this.validateSearchboxInput()) { return; }
+
+      let uri = `json?address=${this.state.searchBoxValue}`;
+      
+      console.log(uri);
+      this.props.fetchLocation(uri);
+   }
+   
+   handleSearchBoxInputChange(input) {
       this.setState({
-         searchBoxInput: event.target.value
+         searchBoxValue: input
       })
    }
-
-   handleKeyPress(event) {
-      if (event.key === "Enter") {
-         this.props.locateCall(this.state.searchBoxInput);
-      }
-   }
-
-   handleSumbitLocation() {
-      this.props.locateCall(this.state.searchBoxInput);
-   }
-
-   handleSubmitGeolocation() {
-      this.props.geoCall();
+   
+   // except for geoBtn, it doesn't need inputfield
+   handleGeoBtnClicked(uri) {
+      this.props.fetchLocation(uri);
    }
 
    render() {
       return (
          <div className="controls">
-            <button name="locater" onClick={this.handleSubmitGeolocation}>
-               <i className="fas fa-location-arrow"></i>
-            </button>
-            
-            {/* // TODO: add google searchbox */}
-            <input
-               autoFocus
-               onChange={this.handleChange}
-               onKeyPress={this.handleKeyPress}
-               onFocus={this.highlightText}
-               id="pac-input"
-               className="searchbox"
-               type="text"
-               placeholder="City, Zip, Locale"
-            />
-
-            <button name="submit-location" onClick={this.handleSumbitLocation}><i className="fas fa-chevron-right"></i></button>
+            <GeoButton onBtnClicked={this.handleGeoBtnClicked}/>            
+            <InputField onSearchBoxInputChange={this.handleSearchBoxInputChange} onKeyPressed={this.handleUserInputs}/>            
+            <LocateButton onBtnClicked={this.handleUserInputs}/>
          </div>
+      )
+   }
+}
+
+class InputField extends React.Component {
+   constructor() {
+      super() 
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+   }
+
+   handleChange(event) {
+      this.props.onSearchBoxInputChange(event.target.value);
+   }
+
+   handleKeyPress(event) {
+      if (event.key === "Enter") {
+         console.log("Pressed enter");
+
+         this.props.onKeyPressed();
+      }
+   }
+
+   render() {
+      return (
+         <input
+            autoFocus
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            onFocus={this.highlightText}
+            id="pac-input"
+            className="searchbox"
+            type="text"
+            placeholder="City, Zip, Locale"
+         />
+      )
+   }
+}
+
+class LocateButton extends React.Component {
+   constructor() {
+      super() 
+
+      this.handleClick = this.handleClick.bind(this);
+   }
+
+   handleClick() {
+      console.log("Locate clicked");
+      
+      this.props.onBtnClicked();
+   }
+
+   render() {
+      return (
+         <button name="submit-location" onClick={this.handleClick}>
+            <i className="fas fa-chevron-right" />
+         </button>         
+      )
+   }
+}
+
+class GeoButton extends React.Component {
+   constructor() {
+      super()
+
+      this.handleClick = this.handleClick.bind(this);
+   }
+
+   handleClick() {
+      let self = this;
+
+      console.log("Geo clicked")
+      function success(pos) {
+         let GeolocateUri = `json?latlng=${pos.coords.latitude},${pos.coords.longitude}`;
+         self.props.onBtnClicked(GeolocateUri);
+      }
+
+      function fail(error) {
+         console.log(error);
+      }
+
+      let options = {
+         timeout: 7500
+      }
+
+      navigator.geolocation.getCurrentPosition(success, fail, options);
+   }
+
+   render() {
+      return (
+      <button name="locater" onClick={this.handleClick}>
+         <i className="fas fa-location-arrow" />
+      </button>
       )
    }
 }
